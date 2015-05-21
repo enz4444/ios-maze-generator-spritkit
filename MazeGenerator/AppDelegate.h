f474dca9ca635925c2ecccaab8d743a7465feae0
Enlan Zhou <zhouenlan4444@yahoo.com>
2015-05-20 20:36:12 -0700

    xcode default game template

---
+++ MazeGenerator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MazeGenerator
+//
+//  Created by littlebeef on 5/20/15.
+//  Copyright (c) 2015 beefSama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MazeGenerator/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  MazeGenerator
+//
+
+//  Copyright (c) 2015 beefSama. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
